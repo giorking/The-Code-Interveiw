05b00850cf5409e6dc54d00f8bcd9ad599411731
Lawrence <jiangyi0425@gmail.com>
2015-07-01 23:50:40 +0800

    Java

--- Cracking-the-coding-interview/Tree&&Map/Question-4-2.java
+++ Cracking-the-coding-interview/Tree&&Map/Question-4-2.java
@@ -1,13 +1,37 @@
 /**
- * Definition for Directed graph.
- * class DirectedGraphNode {
- *     int label;
- *     ArrayList<DirectedGraphNode> neighbors;
- *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
- * };
+ * Created by lawrence on 6/28/15.
  */
+/**
+ * Definition for Directed graph.
+ */
+
+import java.util.*;
+
+class DirectedGraphNode {
+    int label;
+    ArrayList<DirectedGraphNode> neighbors;
+    DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+};
+
+
 public class Solution {
-    public boolean connectedSet2(ArrayList<DirectedGraphNode> nodes, DirectedGraphNode start, DirectedGraphNode end) {    	
+    public boolean connectedSet2(DirectedGraphNode start, DirectedGraphNode end) {
         // Write your code here
+        Set set = new HashSet();
+        set.add(start);
+        LinkedList<DirectedGraphNode> q = new LinkedList<DirectedGraphNode>();
+        q.offer(start);
+        DirectedGraphNode cur;
+        while (!q.isEmpty()) {
+            cur = q.poll();
+            if (cur == end) return true;
+            for (int i = 0; i < cur.neighbors.size(); i ++) {
+                if (!set.contains(cur.neighbors.get(i))) {
+                    set.add(cur.neighbors.get(i));
+                    q.offer(cur.neighbors.get(i));
+                }
+            }
+        }
+        return false;
     }
 }
