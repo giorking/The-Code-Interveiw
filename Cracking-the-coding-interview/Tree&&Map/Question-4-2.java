8136d0f12d8ab17fea8d1ae5fc84ddf00e4efdda
Lawrence <jiangyi0425@gmail.com>
2015-07-01 16:41:43 +0800

    Cpp

---
+++ Cracking-the-coding-interview/Tree&&Map/Question-4-2.java
@@ -0,0 +1,17 @@
+/**
+ * Definition for Directed graph.
+ * class DirectedGraphNode {
+ *     int label;
+ *     ArrayList<DirectedGraphNode> neighbors;
+ *     DirectedGraphNode(int x) { label = x; neighbors = new ArrayList<DirectedGraphNode>(); }
+ * };
+ */
+public class Solution {
+    /**
+     * @param nodes a array of Directed graph node
+     * @return a connected set of a directed graph
+     */
+    public boolean connectedSet2(ArrayList<DirectedGraphNode> nodes, DirectedGraphNode start, DirectedGraphNode end) {    	
+        // Write your code here
+    }
+}
